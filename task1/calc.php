fb271a3b0dd8b82f636f913985f556da4244728f
el3raky-coder <79190513+adel-eleraky@users.noreply.github.com>
2022-10-04 07:08:19 +0200

    Add files via upload

---
+++ task1/calc.php
@@ -0,0 +1,64 @@
+<?php
+
+
+    $result = "";
+    $operation = $_POST['add'] ?? $_POST['subtract'] ?? $_POST['multiply'] ?? $_POST['division'] ?? $_POST['modulus'] ?? "" ;
+    
+    if(!empty( $_POST['num1'] ) and !empty(  $_POST['num2']) ){
+        switch($operation){
+            case "add":
+                $result = $_POST['num1'] + $_POST['num2'];
+                break;
+            case "subtract":
+                $result = $_POST['num1'] - $_POST['num2'];
+                break;
+            case "multiply":
+                $result = $_POST['num1'] * $_POST['num2'];
+                break;
+            case "division":
+                $result = $_POST['num1'] / $_POST['num2'];
+                break;
+            case "modulus":
+                $result = $_POST['num1'] % $_POST['num2'];
+                break;
+        }
+        
+    }
+    $result = "<div class='result'> the result is {$result}
+                </div>";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/calc.css">
+    <title>task1 - grade</title>
+</head>
+
+<body>
+    <div class="container">
+        <form action="" method="POST">
+            <label for="">number 1</label>
+            <input type="number" name="num1" placeholder="enter first number" required>
+            <label for="">number 2</label>
+            <input type="number" name="num2" placeholder="enter second number" required>
+            <button name="add" value="add">add</button>
+            <button name="subtract" value="subtract">subtract</button>
+            <button name="multiply" value="multiply">multiply</button>
+            <button name="division" value="division">division</button>
+            <button name="modulus" value="modulus">modulus</button>
+            <?php 
+                if(!empty($_POST)){
+                    echo $result;
+                }
+            ?>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ task1/grade.php
@@ -0,0 +1,76 @@
+<?php 
+    
+    $percentage = "";
+    $grade = "";
+    if($_POST){
+        $sum = $_POST['math'] + $_POST['physics'] + $_POST['chemistry'] + $_POST['biology'] + $_POST['computer'];
+        $percentage = ( $sum / 500 ) * 100 ;
+    }
+    if($percentage >= 0 and $percentage < 40){
+        $grade = " F";
+    }
+    elseif($percentage >= 40 and $percentage < 60){
+        $grade = " E";
+    }
+    elseif($percentage >= 60 and $percentage < 70){
+        $grade = " D";
+    }
+    elseif($percentage >= 70 and $percentage < 80){
+        $grade = " C";
+    }
+    elseif($percentage >= 80 and $percentage < 90){
+        $grade = " B";
+    }
+    elseif($percentage >= 90 and $percentage <= 100 ){
+        $grade = " A";
+    }
+    $message = "<div class='result'>
+                    percentage: {$percentage}% , Grade: {$grade} 
+                </div>";
+    if($percentage > 100){
+        $message = "error ( wrong marks)";
+    }
+    
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/grade.css">
+    <title>task1 - grade</title>
+</head>
+
+<body>
+    <div class="container">
+        <form action="" method="POST">
+            <label for="">math</label>
+            <input type="number" name="math" required placeholder="enter math degree">
+            <label for="">physics</label>
+            <input type="number" name="physics" required placeholder="enter physics degree">
+            <label for="">chemistry</label>
+            <input type="number" name="chemistry" required placeholder="enter chemistry degree">
+            <label for="">biology</label>
+            <input type="number" name="biology" required placeholder="enter biology degree">
+            <label for="">computer</label>
+            <input type="number" name="computer" required placeholder="enter computer degree">
+            <button>calculate</button>
+        </form>
+        <?php  
+            if($_POST){
+                echo $message;
+            }
+        ?>
+    </div>
+
+
+</body>
+
+</html>
