2a4ecf20c38c69c75b8f248da706460298985570
el3raky-coder <79190513+adel-eleraky@users.noreply.github.com>
2022-10-26 10:03:36 +0200

    Add files via upload

--- dashboard/routes/api.php
+++ dashboard/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Apis\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::controller(ProductController::class)->prefix('dashboard')->name('dashboard.')->group(function(){
+
+    Route::prefix('products')->name('products.')->controller(ProductController::class)->group(function(){
+        Route::get('/' ,  'get')->name('get');   //api/dashboard/products/
+        Route::get('/edit/{id}' ,  'edit' )->name('edit');  //api/dashboard/products/edit/1
+        Route::delete('/delete/{id}' ,'delete' )->name('delete');  //api/dashboard/products/delete/1
+        Route::post('/update/{id}' ,  'update' )->name('update');  //api/dashboard/products/update/1
+        Route::get('/create' , 'create' )->name('create');  //api/dashboard/products/create
+        Route::post('/store' ,  'store' )->name('store');  //api/dashboard/products/store
+    });
 });
